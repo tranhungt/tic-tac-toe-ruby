c669889b2c5f7f0cad5bd74fa07ca62833ee2a1b
Hung Tran <tranhungt1@gmail.com>
2013-11-19 12:44:36 -0800

    new initializer for computer player

--- computer_player.rb
+++ computer_player.rb
@@ -1,8 +1,24 @@
 require_relative 'player'
 
 class ComputerPlayer < Player
+  attr_reader :comp_first, :human_first, :human_symbol
+  def initialize(symbol)
+    human_symbol = symbol == :O ? :X : :O
+    comp_first= get_comp_first_matrix
+    human_first = get_human_first_matrix
+    super(symbol)
+  end
+
   def get_move
     [[0,1,2].sample, [0,1,2].sample]
+  end
+
+  def get_comp_first_matrix
+      
+  end
+
+  def get_human_first_matrix
+
   end
   
 end
