2da4e9e78afc8459054cdff67113430daa8882c6
Hung Tran <tranhungt1@gmail.com>
2013-11-18 16:14:29 -0800

    removes human_player specs

--- spec/human_player_spec.rb
+++ spec/human_player_spec.rb
@@ -3,9 +3,4 @@ require_relative 'spec_helper'
 describe HumanPlayer do
   subject(:player){HumanPlayer.new(:X)}
 
-  describe '#get_move' do
-    it 'gets [y,x] move coord from user' do
-      
-    end
-  end
 end
