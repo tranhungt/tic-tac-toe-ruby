a9b4612262dbea835e5cc15915e9542da8ef65d2
Hung Tran <tranhungt1@gmail.com>
2013-11-19 17:34:01 -0800

    adds point map and initial points array

--- computer_player.rb
+++ computer_player.rb
@@ -2,13 +2,26 @@ require_relative 'player'
 
 class ComputerPlayer < Player
   attr_reader :human_symbol, :board
+  POINT_MAP = {
+    [0,0] => [1,1,0,0,1,0,0,0],
+    [0,1] => [0,1,0,0,0,1,0,0],
+    [0,2] => [0,1,0,0,0,0,1,1],
+    [1,0] => [0,0,1,0,1,0,0,0],
+    [1,1] => [1,0,1,0,0,1,0,1],
+    [1,2] => [0,0,1,0,0,0,1,0],
+    [2,0] => [0,0,0,1,1,0,0,1],
+    [2,1] => [0,0,0,1,0,1,0,0],
+    [2,2] => [1,0,0,1,0,0,1,0]
+  }
   def initialize(symbol, board)
     human_symbol = symbol == :O ? :X : :O
     @board = board
+    current_points = Array.new(8){0}
     super(symbol)
   end
 
   def get_move
+    
     [[0,1,2].sample, [0,1,2].sample]
   end
   
